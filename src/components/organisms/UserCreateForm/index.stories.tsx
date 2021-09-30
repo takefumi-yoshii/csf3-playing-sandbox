14fb15b454a1d0d240bcaa82208030ad71eb7eb8
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-09-30 23:17:45 +0900

    refactor(story): add playFactory

--- src/components/organisms/UserCreateForm/index.stories.tsx
+++ src/components/organisms/UserCreateForm/index.stories.tsx
@@ -7,7 +7,8 @@ type Story = ComponentStoryObj<typeof UserCreateForm>;
 
 export default { component: UserCreateForm };
 
-const type = (step: 1 | 2 | 3) => {
+const type = (step: 0 | 1 | 2 | 3) => {
+  if (step === 0) return;
   userEvent.type(screen.getByPlaceholderText("姓"), "田中");
   if (step === 1) return;
   userEvent.type(screen.getByPlaceholderText("名"), "太郎");
@@ -18,34 +19,28 @@ const type = (step: 1 | 2 | 3) => {
   );
 };
 
+const playFactory = (step: 0 | 1 | 2 | 3) => async () => {
+  type(step);
+  userEvent.click(screen.getByRole("button"));
+};
+
 export const InValid1: Story = {
   storyName: "未入力で送信",
-  play: async () => {
-    userEvent.click(screen.getByRole("button"));
-  },
+  play: playFactory(0),
 };
 
 export const InValid2: Story = {
   storyName: "姓未入力で送信",
-  play: async () => {
-    type(1);
-    userEvent.click(screen.getByRole("button"));
-  },
+  play: playFactory(1),
 };
 
 export const InValid3: Story = {
   storyName: "名未入力で送信",
-  play: async () => {
-    type(2);
-    userEvent.click(screen.getByRole("button"));
-  },
+  play: playFactory(2),
 };
 
 export const Valid: Story = {
   storyName: "正常入力で送信",
   args: { handleSubmit: (data) => {} },
-  play: async () => {
-    type(3);
-    userEvent.click(screen.getByRole("button"));
-  },
+  play: playFactory(3),
 };
